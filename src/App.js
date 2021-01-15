e3ee8185134abd3eb437b8bbd0f5ca0b28048486
Prabh-Sandhu13 <37466719+Prabh-Sandhu13@users.noreply.github.com>
2021-01-15 15:57:18 -0400

    src

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  state = {
+    searchTerm: '',
+    Results:[],
+    Nominations:[]
+  }
+
+  editSearch = (e) => {
+    this.searchMovie(e.target.value)
+    this.setState({searchTerm: e.target.value})
+  }
+
+  searchMovie = (title) => {
+    if (title == ""){
+      this.setState({ Results: [] })
+      this.setState({ Nominations: [] })
+    } else {
+        const url = "http://www.omdbapi.com/?apikey=51fc5dae&t="+title;
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+              if(data.Response =="True") {
+              let newMovie = `${data.Title}(${data.Year})`
+              let movies = this.state.Results
+              if(!movies.includes(newMovie)) {
+                movies.push(newMovie)
+              }
+              this.setState({ Results: movies })
+              }
+            })
+            .catch(console.log)
+      }
+  }
+
+  addNomin = (movie) => {
+    let movieNomins = this.state.Nominations
+    if(!movieNomins.includes(movie)) {
+      movieNomins.push(movie)
+    }
+    this.setState({ Nominations: movieNomins })
+  }
+
+  deleteNomin = (nomination) => {
+    let movieNomins = this.state.Nominations
+    if(movieNomins.includes(nomination)) {
+      movieNomins = movieNomins.filter(function(item) {
+        return item !== nomination
+})
+    }
+    this.setState({ Nominations: movieNomins })
+
+  }
+
+
+    render(){
+      return (
+        <div style = {{textAlign: 'center', paddingTop: '30vh'}}>
+          <h2> Search movie </h2>
+          <input type= 'text' value = {this.state.searchTerm} onChange = {this.editSearch} placeholder = 'Search for a movie!'/>
+          <br></br>
+          <h3>Results:</h3>
+          {
+            this.state.Results.map(movie => {
+              return (<div>{movie}<button onClick={() => this.addNomin(movie)}>Nominate!</button></div>);
+            })
+          }
+          <h3>Nominations:</h3>
+          {
+            this.state.Nominations.map(nomination => {
+              return (<div>{nomination}<button onClick={() => this.deleteNomin(nomination)}>Remove</button></div>);
+            })
+          }
+
+        </div>
+      );
+    }
+}
+
+export default App;
